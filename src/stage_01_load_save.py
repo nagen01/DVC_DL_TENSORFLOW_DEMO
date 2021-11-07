2cc449c54f8445af1e83d7143dbd5c1f691d4ec8
nagen01 <nagendrabahadursingh5005@gmail.com>
2021-11-08 00:09:32 +0530

    stage_01: load and save in progress

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,43 @@
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import logging
+import os
+import tqdm
+import shutil
+
+logging_str = "[%(asctime)s: %(lavelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level=logging.INFO, 
+                    format=logging_str, filemode="a")
+
+
+def copy_data(source_dir, target_dir):
+    list_of_files = os.listdir(source_dir)
+    N = len(list_of_files)
+    for file in tqdm(list_of_files, total=N, desc=f"coppying file from {source_dir} to {target_dir}", colour="green"):
+        src = os.path.join(source_dir, file)
+        dest = os.path.join(target_dir, file)
+        shutil.copy(src,dest)
+
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    source_download_dirs = config["source_download_dirs"]
+    local_data_dirs = config["local_data_dirs"]
+
+    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc="list_of_folders", colour="red"):
+        create_directory([local_data_dir])
+        copy_data(source_download_dir, local_data_dir)
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParse()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    parsed_args = args.parse_args()
+    try:
+        logging.info(">>>>> Stage {Stage} started ")
+        get_data(config_path=parsed_args.config)
+        logging.info("Stage {Stage} completed! All the data is saved in local >>>>>")
+    except Exception as e:
+        logging.exception(e)
+        raise e
---
+++ src/utils/all_utils.py
@@ -0,0 +1,15 @@
+import yaml
+import logging
+import os
+
+def read_yaml(path_to_yaml:str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml file {path_to_yaml} loade successfully")
+    return content
+
+def create_directory(dirs:list):
+    for dir in dirs:
+        os.mkdir(dir, exist_ok=True)
+        logging.info(f"directory is created at {dir}")
+
