19134028dae94b8e174f63435f0fa4a940401f54
nagen01 <nagendrabahadursingh5005@gmail.com>
2021-11-09 00:46:52 +0530

    stage_02: base model prepared, ready for training

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -2,13 +2,14 @@ import os
 import logging
 import argparse
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
+import io
 
-logging_str = "[%(acstime)s: %(levelname)s: %(module)s: %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level=logging.INFO, format=logging_str, filemode="a")
-
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level=logging.INFO, 
+                    format=logging_str, filemode="a")
 Stage = "Two"
 
 def prepare_base_model(config_path, params_path):
@@ -24,6 +25,26 @@ def prepare_base_model(config_path, params_path):
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
     model = get_VGG_16_model(input_shape = params["IMAGE_SIZE"], model_path = base_model_path)
+
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freez_all=True,
+        freez_till=None,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    #log model parameters is pending
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    updated_base_model_path = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL_NAME"])
+    full_model.save(updated_base_model_path)
 
 if __name__ ==  "__main__":
     args = argparse.ArgumentParser()
--- src/utils/models.py
+++ src/utils/models.py
@@ -11,3 +11,26 @@ def get_VGG_16_model(input_shape, model_path):
     model.save(model_path)
     logging.info(f"VGG16 base model saved at {model_path}")
     return model
+
+def prepare_model(model, CLASSES, freez_all, freez_till, learning_rate):
+    if freez_all:
+        for layer in model.layers:
+            layer.trainable = False
+    elif(freez_till is not None) and (freez_till > 0):
+        for layer in model.layers[:-freez_till]:
+            layer.trainable = False
+        
+    ## Add our fully connected layers
+    flatten_in = tf.keras.layers.Flatten()(model.output)
+    prediction = tf.keras.layers.Dense(units=CLASSES, activation="softmax")(flatten_in)
+    full_model = tf.keras.models.Model(inputs = model.input, outputs = prediction)
+    full_model.compile(
+        optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate),
+        loss = tf.keras.losses.CategoricalCrossentropy(),
+        metrics = ["accuracy"]
+    )
+
+    logging.info("custom model is compiled and ready to be trained")
+    full_model.summary()
+    return full_model
+
